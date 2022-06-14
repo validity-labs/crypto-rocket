733eed90ecdc105154fb806ab2404a60b6162fce
Antarya <antarya@gmail.com>
2022-06-14 10:56:59 +0400

    feat(ui): changed cypress test configuration

--- packages/awino-ui/testing/cypress/integration/pages/landing.spec.tsx
+++ packages/awino-ui/testing/cypress/integration/pages/landing.spec.tsx
@@ -18,36 +18,41 @@ describe('Root Page', () => {
     cy.get('main').within(() => {
       const section = (n: number) => cy.get(`>section:nth-child(${n})`);
 
+      // TotalSection ONLY WHEN CONNECTED
+
       // StatsSection
-      section(1).find('.badges li img').should('have.length', 4);
-      section(1).find('h1').should('have.length', 1);
-      section(1).find('.card').should('have.length', 4);
+      section(2).find('h1').should('have.length', 1);
+      section(2).find('.card').should('have.length', 4);
 
       // InfoSection
-      section(2).find('h2').should('have.length', 1);
-      section(2).find('p').should('have.length', 1);
-      section(2).find('img').should('have.length', 2);
+      section(3).find('h2').should('have.length', 1);
+      section(3).find('p').should('have.length', 7);
+      section(3).find('[role="tabpanel"]').should('have.length', 6);
 
       // BenefitSection
-      section(3).find('h2').should('have.length', 1);
-      section(3).find('h3').should('have.length', 3);
-      section(3).find('p').should('have.length', 4);
+      section(4).find('h2').should('have.length', 1);
+      section(4).find('h3').should('have.length', 5);
+      section(4).find('p').should('have.length', 6);
+      section(4).find('.swiper').should('have.length', 1);
+      section(4).find('.swiper-slide').should('have.length', 5);
 
       // GuideSection
-      section(4).find('h2').should('have.length', 1);
-      section(4).find('h3').should('have.length', 3);
-      section(4).find('p').should('have.length', 9);
-      section(4).find('img').should('have.length', 6);
+      section(5).find('h2').should('have.length', 1);
+      section(5).find('h3').should('have.length', 3);
+      section(5).find('p').should('have.length', 9);
+      section(5).find('img').should('have.length', 6);
 
       // AssetSection
-      section(5).find('h2').should('have.length', 1);
-      section(5).find('p').should('have.length', 1);
-      section(5).find('img').should('have.length', 8);
+      // section(6).find('h2').should('have.length', 1);
+      // section(6).find('p').should('have.length', 6);
+      // section(6).find('img').should('have.length', 8);
 
       // FAQSection
       section(6).find('h2').should('have.length', 1);
       section(6).find('p').should('have.length.gte', 2);
-      section(6).find('.MuiAccordion-root').should('have.length', 4);
+      section(6).find('.MuiAccordion-root').should('have.length', 5);
+
+      // JoinSection ONLY WHEN CONNECTED
     });
   });
 });
