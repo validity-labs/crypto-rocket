092ab36e6924b268709f2aabb4ce719a82634c83
kpaschalidis <konstantinos.paschalidis@validitylabs.org>
2022-07-05 18:38:06 +0200

    WIP - Update MasterChef contract to V2

--- packages/awino-contracts/farm-pools/tasks/create-farm-pool.ts
+++ packages/awino-contracts/farm-pools/tasks/create-farm-pool.ts
@@ -9,16 +9,13 @@ task("create-farm-pool")
   )
   .addParam("allocPoints", "Allocation points", undefined, types.string)
   .addParam("lpToken", "LP token address", undefined, types.string)
+  .addOptionalParam("isRegular", "Is regular", true, types.boolean)
   .addOptionalParam("withUpdate", "WithUpdate", true, types.boolean)
   .setAction(async (args, { ethers }) => {
-    const masterChefFactory = await ethers.getContractFactory("MasterChef");
+    const masterChefFactory = await ethers.getContractFactory("MasterChefV2");
     const masterChef = masterChefFactory.attach(args.masterChef);
 
-    const tx = await masterChef.add(
-      args.allocPoint,
-      args.lpToken,
-      args.withUpdate
-    );
+    const tx = await masterChef.add(0, args.lpToken, true, args.withUpdate);
 
     await tx.wait(1);
 
